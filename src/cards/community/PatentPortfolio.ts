1a6396b50a3ee02ae421154adba9923451245e6f
puuj <github@puuj.org>
2021-05-25 09:49:43 -0700

    Fix logging

--- src/cards/community/PatentPortfolio.ts
+++ src/cards/community/PatentPortfolio.ts
@@ -4,7 +4,6 @@ import {CardType} from '../CardType';
 import {Player} from '../../Player';
 import {Card} from '../Card';
 import {CardName} from '../../CardName';
-import {LogHelper} from '../../LogHelper';
 import {Resources} from '../../Resources';
 import {CardRenderer} from '../render/CardRenderer';
 
@@ -21,14 +20,13 @@ export class PatentPortfolio extends Card implements IProjectCard {
         renderData: CardRenderer.builder((b) => {
           b.megacredits(1).slash().cards(1);
         }),
-        description: 'Gain 1 MC for each card in hand',
+        description: 'Gain 1 M€ for each card in hand',
       },
     });
   }
 
   public play(player: Player) {
-    player.megaCredits += player.cardsInHand.length;
-    LogHelper.logGainStandardResource(player, Resources.MEGACREDITS, player.cardsInHand.length);
+    player.addResource(Resources.MEGACREDITS, player.cardsInHand.length, {log: true});
     return undefined;
   }
 }
