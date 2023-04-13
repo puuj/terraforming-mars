870db85e0203c84cf5261d3775b1d9f57a263170
puuj <github@puuj.org>
2023-04-12 18:57:51 -0700

    Fix the VP definition for HappyBirthday

--- src/server/cards/community/HappyBirthday.ts
+++ src/server/cards/community/HappyBirthday.ts
@@ -5,6 +5,7 @@ import {CardType} from '../../../common/cards/CardType';
 import {CardName} from '../../../common/cards/CardName';
 import {CardRenderer} from '../render/CardRenderer';
 import {CardResource} from '../../../common/CardResource';
+import {VictoryPoints} from '../ICard';
 
 export class HappyBirthday extends Card implements IProjectCard {
   constructor() {
@@ -15,7 +16,7 @@ export class HappyBirthday extends Card implements IProjectCard {
       cost: 0,
 
       resourceType: CardResource.CANDLE,
-      victoryPoints: {type: 'resource', points: 1, per: 1},
+      victoryPoints: VictoryPoints.resource(1, 1),
       
       behavior: {
         addResources: 37,
@@ -23,12 +24,12 @@ export class HappyBirthday extends Card implements IProjectCard {
 
       metadata: {
         cardNumber: 'J33',
-        description: 'Add 1 candle resource to this card per year of your life.',
         renderData: CardRenderer.builder((b) => {
           b.candle().slash().text("year");
           b.br;
           b.vpText('1 VP per candle resource on this card.');
         }),
+        description: 'Add 1 candle resource to this card per year of your life.',
       },
     });
   }
