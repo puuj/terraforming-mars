9215c6343eb377c5e6e4b3dd4d6f109bb0743798
puuj <github@puuj.org>
2024-06-21 13:49:01 -0700

    make merger more apparent

--- src/client/components/GameSetupDetail.vue
+++ src/client/components/GameSetupDetail.vue
@@ -100,8 +100,8 @@
               <div v-if="gameOptions.showTimers" class="game-config timer" v-i18n>timer</div>
               <div v-if="gameOptions.showOtherPlayersVP" class="game-config realtime-vp" v-i18n>real-time vp</div>
               <div v-if="gameOptions.undoOption" class="game-config undo" v-i18n>undo</div>
+              <div v-if="gameOptions.twoCorpsVariant" class="game-config generic" v-i18n>Merger</div>
             </li>
-            <li v-if="gameOptions.twoCorpsVariant"><div class="setup-item" v-i18n>Merger</div></li>
             <li v-if="gameOptions.bannedCards.length > 0"><div class="setup-item" v-i18n>Banned cards:</div>{{ gameOptions.bannedCards.join(', ') }}</li>
           </ul>
         </div>
