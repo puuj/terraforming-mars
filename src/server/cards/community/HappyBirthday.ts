4905c8e69a0451e77fcf2c55fa6e2c7576338d10
puuj <github@puuj.org>
2026-04-26 00:26:35 -0700

    update candles

--- src/server/cards/community/HappyBirthday.ts
+++ src/server/cards/community/HappyBirthday.ts
@@ -18,7 +18,7 @@ export class HappyBirthday extends Card implements IProjectCard {
       victoryPoints: {resourcesHere: {}},
 
       behavior: {
-        addResources: 44,
+        addResources: 43,
       },
 
       metadata: {
